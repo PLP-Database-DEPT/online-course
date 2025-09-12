77b0378a61356bd0cc11085c05d4f6219c05ce8d
Noah Ewalan <106330276+ramnoa@users.noreply.github.com>
2025-09-12 09:23:36 +0300

    added enrollment table

--- db.sql
+++ db.sql
@@ -2,3 +2,12 @@
 -- create database
 CREATE DATABASE course;
 USE course;
+-- enrolment table with foreign keys referencing course and student tables
+CREATE TABLE enrolment (
+    course_id INT,
+    student_id INT,
+    enrolment_datetime DATETIME,
+    completed_datetime DATETIME,
+    FOREIGN KEY (course_id) REFERENCES course(id),
+    FOREIGN KEY (student_id) REFERENCES student(id)
+);
