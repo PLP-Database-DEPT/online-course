8ce4fa8ad4aa4d0a39328f319213cdbbd8863e3c
Simret <simruineb@gmail.com>
2025-09-16 16:19:36 +0300

    Create student table in with the required columns

--- db.sql
+++ db.sql
@@ -1,4 +1,7 @@
+-- Table: student to store student login credentials and identification details.
 
--- create database
-CREATE DATABASE course;
-USE course;
+CREATE TABLE student (
+    id INT PRIMARY KEY,
+    email_address VARCHAR(100) NOT NULL,
+    password VARCHAR(200) NOT NULL
+);
