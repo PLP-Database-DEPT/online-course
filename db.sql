30ce9090eec26d9362776316e4774ad08ee59971
3ugenesGit <150066288+3ugenesGit@users.noreply.github.com.>
2025-09-17 15:39:44 -0700

    Created the course table

--- db.sql
+++ db.sql
@@ -1,4 +1,15 @@
-
 -- create database
 CREATE DATABASE course;
 USE course;
+-- create course table
+CREATE TABLE Course(
+    id INT PRIMARY KEY,
+    --Primary Key
+    name VARCHAR(100) NOT NULL,
+    -- Name of the Course
+    description VARCHAR(255),
+    -- Detailed description of the course
+    price INT,
+    -- Cost of the course 
+    is_progress_limited TINYINT -- Indicates whether course progress is limited by progress/time
+);
